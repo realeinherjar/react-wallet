cdfd5117ba44b65ace5632d8136a7299081ee5e2
Einherjar <realeinherjar@proton.me>
2023-08-20 09:42:11 -0300

    feat(config): static export for SPA in GH Pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  /*
+  Deploy to static site or Single-Page Application (SPA),
+  then later optionally upgrading to use features that require a server.
+  Currently setup for GitHub Pages.
+  */
+  output: "export",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
