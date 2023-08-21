c00b37c683f44a35516e3fb8480f19d6924b43ed
Einherjar <realeinherjar@proton.me>
2023-08-21 07:46:15 -0300

    feat(AddressTable): add `AddressTable` component (#1)
    feat(AddressTable): add AddressTable component
    
    - table with address, confirmed balance, pending balance, and total balance
    - Esplora API utils functions to get balance from an address

---
+++ app/components/AddressTable.tsx
@@ -0,0 +1,40 @@
+"use server";
+import { getBalance, Address } from "../utils/esplora";
+export default async function AddressTable() {
+  // TODO: delete fake data, this will be populated by the API
+  const fakeAddress: string[] = [
+    "tb1phfzpptykdhs6803zwa9sprhjhlr7lwp94matqugwrx3qh50c33jsl9a8kp",
+    "tb1p04q02c56dqdn786yle9kgwymzquellpp3x6jg5znthhytxm9frnspddmmr",
+    "tb1p4km97uu685e7knem0umv4jyul0dwghff5xxdzfl2ujtadeln6tns702h80",
+    "tb1ph3q0utagzyylk6qxuaf98yzgfteesfmuajs5y3l5gyr4f8ktsldqp6rwmn",
+  ];
+
+  const addresses: Address[] = await Promise.all(
+    fakeAddress.map((addr) => getBalance(addr))
+  );
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Address</th>
+          <th>Balance (Confirmed)</th>
+          <th>Balance (Pending)</th>
+          <th>Balance (Total)</th>
+        </tr>
+      </thead>
+      <tbody>
+        {addresses
+          .filter((addr) => addr.total > 0)
+          .map((addr) => (
+            <tr key={addr.address}>
+              <td>{addr.address}</td>
+              <td>{addr.balance}</td>
+              <td>{addr.pending}</td>
+              <td>{addr.total}</td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  );
+}
