1e5c64b92f28fd516ed88c5cf16076c0ab061378
Einherjar <realeinherjar@proton.me>
2023-08-22 10:07:35 -0300

    fix: refactor

--- lib/bitcoin.ts
+++ lib/bitcoin.ts
@@ -1,26 +1,32 @@
 import { mnemonicToSeedSync } from "bip39";
-import { BIP32Factory } from "bip32";
 import * as ecc from "tiny-secp256k1";
 import { networks, payments, initEccLib } from "bitcoinjs-lib";
 
 const bip32 = BIP32Factory(ecc);
 initEccLib(ecc);
 
-// export function getReceiveAddress(mnemonic: string, index: string) { // TODO: handle mnemonic phrase
-export function getReceiveAddress(index: number, network: string): string {
-  // TODO: handle mnemonic phrase
-  const mnemonic: string =
-    "before memory calm clutch march pact useless transfer suffer vehicle already toss cave alley tumble foil volume patch cave royal weekend frost inmate catalog";
+// TODO: handle mnemonic phrase
+const mnemonic: string =
+  "before memory calm clutch march pact useless transfer suffer vehicle already toss cave alley tumble foil volume patch cave royal weekend frost inmate catalog";
 
-  const network_type =
-    network === "testnet" ? networks.testnet : networks.bitcoin;
+// hardcode the derivaton path to taproot
+const path: string = "m/86'/1'/0'/";
 
-  // hardcode the derivaton path to taproot
-  const outerPath: string = `m/86'/1'/0'/0/${index}`;
-
+// function getChildNode(mnemonic: string, type: string, index: number): BIP32Interface  {
+function getChildNode(type: string, index: number): BIP32Interface {
+  const path_type: string = type === "receive" ? "0" : "1";
+  const childPath: string = `${path}${path_type}/${index}`;
   const seed = mnemonicToSeedSync(mnemonic);
   const node = bip32.fromSeed(seed);
-  const childNode = node.derivePath(outerPath);
+  const childNode = node.derivePath(childPath);
+  return childNode;
+}
+
+// export function getReceiveAddress(mnemonic: string, index: string, network: string) { // TODO: handle mnemonic phrase
+export function getReceiveAddress(index: number, network: string): string {
+  const network_type =
+    network === "testnet" ? networks.testnet : networks.bitcoin;
+  const childNode = getChildNode("receive", index);
 
   const address = payments.p2tr({
     internalPubkey: childNode.publicKey.subarray(1),
@@ -34,21 +40,11 @@ export function getReceiveAddress(index: number, network: string): string {
   return address;
 }
 
-// export function getChangeAddress(mnemonic: string, index: string) { // TODO: handle mnemonic phrase
+// export function getChangeAddress(mnemonic: string, index: string, network: string) { // TODO: handle mnemonic phrase
 export function getChangeAddress(index: number, network: string): string {
-  // TODO: handle mnemonic phrase
-  const mnemonic: string =
-    "before memory calm clutch march pact useless transfer suffer vehicle already toss cave alley tumble foil volume patch cave royal weekend frost inmate catalog";
-
   const network_type =
     network === "testnet" ? networks.testnet : networks.bitcoin;
-
-  // hardcode the derivaton path to taproot
-  const outerPath: string = `m/86'/1'/0'/1/${index}`;
-
-  const seed = mnemonicToSeedSync(mnemonic);
-  const node = bip32.fromSeed(seed);
-  const childNode = node.derivePath(outerPath);
+  const childNode = getChildNode("change", index);
 
   const address = payments.p2tr({
     internalPubkey: childNode.publicKey.subarray(1),
